2a882a7d7f0dd6f1655dea71ba46f0521d607158
Ben Kallus <benjamin.p.kallus.gr@dartmouth.edu>
2024-04-21 20:59:08 -0400

    Fix stuff in ipv6

--- ipv6.py
+++ ipv6.py
@@ -30,11 +30,12 @@ class IPv6Packet:
     |                                                               |
     +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
     """
-    version: int  # = 6
+
+    version: int
     traffic_class: int
     flow_label: int
     payload_length: int
-    next_header: int # Same as ipv4 protocol field
+    next_header: int  # Same as ipv4 protocol field
     hop_limit: int
     source_address: IPv6Address
     destination_address: IPv6Address
@@ -46,19 +47,25 @@ class IPv6Packet:
     def __post_init__(self) -> None:
         assert all(
             (
-            0 <= self.version < 2**4,
-            0 <= self.traffic_class < 2**8,
-            0 <= self.flow_label < 2**20,
-            self.payload_length_is_in_range(),
-            0 <= self.next_header < 2**8,
-            0 <= self.hop_limit < 2**8,
+                0 <= self.version < 2**4,
+                0 <= self.traffic_class < 2**8,
+                0 <= self.flow_label < 2**20,
+                self.payload_length_is_in_range(),
+                0 <= self.next_header < 2**8,
+                0 <= self.hop_limit < 2**8,
             )
         )
 
     def serialize(self) -> bytes:
         result: bytes = b"".join(
             [
-                bytes([(self.version << 28) | (self.traffic_class << 20) | self.flow_label]),
+                bytes(
+                    [
+                        (self.version << 4) | (self.traffic_class >> 4),
+                        ((self.traffic_class & 0xF) << 4) | self.flow_label >> 16,
+                    ]
+                ),
+                int_to_bytes(self.flow_label & 0xFFFF, 2),
                 int_to_bytes(self.payload_length, 2),
                 int_to_bytes(self.next_header, 1),
                 int_to_bytes(self.hop_limit, 1),
@@ -73,15 +80,15 @@ class IPv6Packet:
     def deserialize(cls, data: bytes):
         assert len(data) >= 40
         return cls(
-            (data[0] >> 4) & 0xf,
-            ((data[0] & 0xf) << 4) | ((data[1] & 0xf0) >> 4),
-            ((data[1] & 0xf) << 16) | bytes_to_int(data[2:4]),
+            data[0] >> 4,
+            ((data[0] & 0xF) << 4) | (data[1] >> 4),
+            ((data[1] & 0xF) << 16) | bytes_to_int(data[2:4]),
             bytes_to_int(data[4:6]),
-            bytes_to_int(data[6:7]),
-            bytes_to_int(data[7:8]),
+            data[6],
+            data[7],
             IPv6Address(data[8:24]),
             IPv6Address(data[24:40]),
-            data[40:]
+            data[40:],
         )
 
     def fix_payload_length(self) -> None:
